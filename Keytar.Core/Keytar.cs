352177cb87116900c3e6927687e81855ccf2e9e3
Bogdan Radu <radu.bogdan17@gmail.com>
2020-01-22 22:00:08 +0200

    Keytar Core

--- Keytar.Core/Keytar.cs
+++ Keytar.Core/Keytar.cs
@@ -1,8 +1,34 @@
-﻿using System;
+﻿using System.Runtime.InteropServices;
 
 namespace Keytar.Core
 {
     public class Keytar
     {
+        public static string GetPassword(string service)
+        {
+            var result = get_password(service);
+            return result;
+        }
+
+        public static bool CreatePassword(string service, string account, string password)
+        {
+            var result = create_password(service, account, password);
+            return result == 0;
+        }
+
+        public static bool DeletePassword(string service, string account)
+        {
+            var result = delete_password(service, account);
+            return result == 0;
+        }
+
+        [DllImport("Resources/keytar.dll")]
+        private static extern string get_password(string str);
+
+        [DllImport("Resources/keytar.dll")]
+        private static extern int create_password(string service, string account, string password);
+
+        [DllImport("Resources/keytar.dll")]
+        private static extern int delete_password(string service, string account);
     }
 }
